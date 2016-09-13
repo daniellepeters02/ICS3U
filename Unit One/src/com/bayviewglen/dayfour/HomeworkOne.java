b5a0283e035a322dddf3f0e690db9fa2b3a4440b
dpeters <dpeters@loaner110sp3.bayviewglen.ca>
2016-09-13 13:05:40 -0400

    Homework Day Four

--- Unit One/src/com/bayviewglen/dayfour/HomeworkOne.java
+++ Unit One/src/com/bayviewglen/dayfour/HomeworkOne.java
@@ -4,6 +4,10 @@ public class HomeworkOne {
 
 	public static void main(String[] args) {
 		
+		int number = 4;
+		
+		System.out.println("Number = " +4);
+		System.out.println(number + " squared  = " + number*number);
 
 	}
 
